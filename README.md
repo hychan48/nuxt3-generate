1ca6e1e90a9436088b7ce6f66720587230f846b5
JC <jchan48@gmail.com>
2022-12-18 20:22:25 -0500

    work around for hooks didnt work

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
 
+https://github.com/hychan48/nuxt3-generate
+.nojekyll
+works with .nojekyll
+github pages ignores _files?
+
+
 ## Setup
 
 Make sure to install the dependencies:
--- nuxt.config.mjs
+++ nuxt.config.mjs
@@ -2,11 +2,23 @@
 export default defineNuxtConfig({
   ssr:false,
   target:"static",
+  vite: {
+    base: process.env.NUXT_APP_BASE_URL || '/',
+  },
   nitro:{
     serveStatic:true,
-    baseURL:'/a/',
+    baseURL:process.env.NUXT_APP_BASE_URL,
+    // buildDir:"nitroBuildDir",//cant override this either, so they're doing this wrong
+    //nuxt 3 problem i think
     logLevel:3,
-    preset:"node-server",
+    // logLevel:1,
+    // preset:"node-server",
+    // output: {
+    // //   dir: 'mydir',
+    // // //   publicDir: '.output/something',
+    //   publicDir: 'output/public'
+    //   // //   serverDir:'.output/server',
+    // },
   },
   //https://github.com/TimonLukas/nuxt-prerender-baseurl-reproduction/blob/master/nuxt.config.ts
   // hooks: {
@@ -26,7 +38,7 @@ export default defineNuxtConfig({
   //   }
   // },
 })
-console.log('sup',process.env.NITRO_PORT);
+// console.log('sup',process.env.NITRO_PORT);
 /**
  *
  * http://localhost:3000/_payload.js
